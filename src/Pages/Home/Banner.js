ffe59bc2a6afa7d09ede918394b19994bd6cb7c6
pritom48 <pritamdas41148@gmail.com>
2022-06-09 12:38:07 +0600

    first commit

---
+++ src/Pages/Home/Banner.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import mypic from '../../img/mypic.png'
+
+const Banner = () => {
+    return (
+        <div class="hero min-h-screen bg-base-200">
+            <div class="hero-content flex-col lg:flex-row-reverse">
+                <img width={500} src={mypic} alt='' />
+                <div>
+                    <h1 class="text-5xl font-bold text-white">HI, I AM PRITOM</h1>
+                    <h1 className='text-3xl py-3'>MREN Stack developer</h1>
+                    <p class="py-3 text-white">Hello, I am a Junior Web Developer. I have been working in this field for the last six months. I love being in this line of work because I am passionate about design, development, ReactJs, Javascript, and so on. I have completed many projects in the last six months.</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/Shared/Navbar.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+
+const Navbar = () => {
+
+    const manuitem = <>
+        <li><a>Home</a></li>
+        <li><a>Service</a></li>
+        <li><a>About Me</a></li>
+        <li><a>Contact Me</a></li>
+    </>
+
+    return (
+        <div class="navbar bg-base-100">
+            <div class="navbar-start">
+                <div class="dropdown">
+                    <label tabindex="0" class="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        {manuitem}
+                    </ul>
+                </div>
+                <a class="btn btn-ghost normal-case text-xl">PRITOM DAS</a>
+            </div>
+            <div class="navbar-center hidden lg:flex">
+                <ul class="menu menu-horizontal p-0">
+
+                    {manuitem}
+                </ul>
+            </div>
+            <div class="navbar-end">
+                <button
+                    class="btn btn-outline">
+                    <a href={require("../img/PRITOM_DAS_JUINOR_WEB_DEVELOPER.pdf")} download="Pritom_Resume">Download Resume</a>
+                </button>
+
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
