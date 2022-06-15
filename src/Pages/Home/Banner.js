c1f5a816f4fc4b3bcc4714555a86e504a3ddb122
pritom48 <pritamdas41148@gmail.com>
2022-06-15 20:53:56 +0600

    typewritereffect added

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import TypeWriterEffect from 'react-typewriter-effect';
 import mypic from '../../img/mypic.png'
 import facebook from '../../img/icon/facebook.png'
 import linkedin from '../../img/icon/linkedin1.png'
@@ -10,8 +11,25 @@ const Banner = () => {
             <div class="hero-content flex-col lg:flex-row-reverse">
                 <img width={500} src={mypic} alt='' />
                 <div>
-                    <h1 class="text-5xl font-bold text-white">HI, I AM PRITOM</h1>
-                    <h1 className='text-3xl py-3'>MREN Stack developer</h1>
+                    <h1 class="text-5xl font-bold text-white pb-3">HI, I AM PRITOM</h1>
+                    <TypeWriterEffect
+                        textStyle={{
+                            fontFamily: 'Red Hat Display',
+                            color: 'orange',
+                            fontWeight: 500,
+                            fontSize: '2.5em',
+                        }}
+                        startDelay={''}
+                        cursorColor="#ffff"
+                        multiText={[
+                            'MREN Stack developer',
+                            'React Developer',
+                            'Front-end Developer'
+                        ]}
+                        multiTextDelay={2000}
+                        typeSpeed={100}
+                        multiTextLoop
+                    />
                     <p class="py-3 text-white">Hello, I am a Junior Web Developer. I have been working in this field for the last six months. I love being in this line of work because I am passionate about design, development, ReactJs, Javascript, and so on. I have completed many projects in the last six months.</p>
                     <div className='flex mt-5'>
                         <a className='ml-2' target="_blank" href="https://www.linkedin.com/in/pritom-das/"> <img width={50} src={linkedin} alt="" /></a>
